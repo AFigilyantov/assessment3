40f97e57085798cb85ca7ab40090540d6d7b629e
AFigilyantov <161585369+AFigilyantov@users.noreply.github.com>
2024-10-03 15:31:28 +0500

    Revert "Server for assesment"

--- generate.go
+++
@@ -1,4 +0,0 @@
-package main
-
-//go:generate oapi-codegen --config=oapi_server.config.yaml api/openapi/swagger.yaml
-//go:generate oapi-codegen --config=oapi_models.config.yaml api/openapi/swagger.yaml
--- internal/auth/entity/entity.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-// UserAccount - db schema
-type UserAccount struct {
-	Username  string `json:"username" validate:"required,min=1,max=20"`
-	Password  string `json:"password" validate:"required,min=1"`
-	Email     string `json:"email" validate:"required,email"`
-	CreatedAt string
-}
